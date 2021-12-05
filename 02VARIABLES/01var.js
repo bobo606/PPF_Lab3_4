bd6cdcd7390440a7b0445aeccd354db4580192ad
Kacper Kawula <kacperbw@gmail.com>
2021-12-05 08:04:24 +0100

    dom

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,30 @@
+var x = 4;
+var y = 6;
+var z = x + y;
+
+var nameq ="Kacper";
+
+
+
+var person = "Karolina Kowalska", carName = "Nissan", price = 20000;
+
+var personA = "Karolina Kowalska",
+carNameA = "Nissan",
+priceA = 20000;
+
+// zmiennna zadeklarowana bez wartości będzie miała wartość undefined.
+var carNameB;
+var personX= "Janina" + " " + "Nowak";
+
+var isFamous = true; //false 
+var xx = 2 + 3 + "5";
+
+
+console.log(x,y,z)
+console.log(nameq)
+console.log(person,carName,price)
+console.log(personA,carNameA,priceA)
+console.log(carNameB)
+console.log(personX)
+console.log(isFamous)
+console.log(xx)
